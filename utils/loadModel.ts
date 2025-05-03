63fd69bd0a6e71f9940d847ae8fd279841940e25
Roxy-Hart <roxyhart03@gmail.com>
2025-05-03 20:55:09 +0530

    Update loadModel.ts

--- utils/loadModel.ts
+++ utils/loadModel.ts
@@ -1,7 +1,8 @@
+// utils/loadModel.ts
 import * as tf from '@tensorflow/tfjs';
 
 export async function loadModel() {
-  const model = await tf.loadGraphModel("/radiator_model/model.json");
+  const model = await tf.loadGraphModel('/radiator_model/model.json');
   console.log('Model loaded successfully');
   return model;
 }
